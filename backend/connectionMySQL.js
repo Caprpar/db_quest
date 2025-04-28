51221267af47253e6068e400a21e8e61ad4bbeaa
Caspar Danielsson <caspar.danielsson@iths.se>
2025-04-28 15:51:00 +0200

    update .gitignore

--- backend/connectionMySQL.js
+++
@@ -1,9 +0,0 @@
-const mysql = require("mysql2");
-const connectionMySQL = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "password",
-  database: "flora",
-});
-
-module.exports = connectionMySQL;
