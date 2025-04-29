03315de02f682f9ecb506adde429f983d7cd51ad
Caspar Danielsson <caspar.danielsson@iths.se>
2025-04-29 15:40:41 +0200

    begin controllers

--- backend/controllers/cardController.js
+++ backend/controllers/cardController.js
@@ -1,1 +1,34 @@
 const cardService = require("../services/cardService");
+
+exports.getCards = async (req, res) => {
+  try {
+    const cards = await cardService.xx();
+    res.status(200).json({ cards });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+exports.getCardBy = async (req, res) => {
+  try {
+    const card = await cardService.xx();
+    res.status(200).json({ card });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+exports.createCard = async (req, res) => {
+  try {
+    const createdCard = await cardService.xx();
+    res.status(200).json({ createdCard });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+exports.deleteCardById = async (req, res) => {
+  try {
+    const deletedCard = await cardService.xx();
+    res.status(200).json({ deletedCard });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
--- backend/routes/cardRoutes.js
+++ backend/routes/cardRoutes.js
@@ -4,6 +4,7 @@ const cardController = require("../controllers/cardController");
 
 router.get("/cards", cardController.getCards());
 router.get("/cards/:id", cardController.getCardBy(id));
-router.post("/cards", cardController.createCard(cType, cName, cScore, cDescription));
+router.post("/cards", cardController.createCard(cType, cName, cScore, (cDescription = "")));
+router.delete("/cards", cardController.deleteCardById(id));
 
 module.exports = router;
