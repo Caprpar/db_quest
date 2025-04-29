316b626fc0f2724c7db8b0f9e9fae3bd2664b7fd
swoerve <swoerve@gmail.com>
2025-04-29 15:29:36 +0200

    remove old connect

--- backend/connectMySQL.js
+++
@@ -1,9 +0,0 @@
-const mysql = require("mysql2")
-const connectionMySQL = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "password",
-  database: "db_quest",
-})
-
-module.exports = connectionMySQL
