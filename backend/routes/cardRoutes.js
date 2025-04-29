9d042aaf670d3bf148e7512ad0d9b98ee81c8429
Caspar Danielsson <caspar.danielsson@iths.se>
2025-04-29 16:20:44 +0200

    post and get works

--- backend/routes/cardRoutes.js
+++ backend/routes/cardRoutes.js
@@ -3,8 +3,8 @@ const router = express.Router();
 const cardController = require("../controllers/cardController");
 
 router.get("/cards", cardController.getCards);
-router.get("/cards/:id", cardController.getCardBy);
+router.get("/cards/:id", cardController.getCardById);
 router.post("/cards", cardController.createCard);
-router.delete("/cards", cardController.deleteCardById);
+router.delete("/cards/:id", cardController.deleteCardById);
 
 module.exports = router;
