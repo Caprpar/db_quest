0a3595f769a403bc708103129d0d941d7a4a05ed
gapsofmind <morin.ibrahim@gmail.com>
2025-04-30 14:40:09 +0200

    added POST and GET for scenes

--- backend/routes/sceneRoutes.js
+++ backend/routes/sceneRoutes.js
@@ -1,7 +1,11 @@
-/* const express = require("express");
+const express = require("express");
 const router = express.Router();
-const sceneController = require("../controllers/templateController");
+const sceneController = require("../controllers/sceneController");
+
+router.get("/scenes", sceneController.getScenes);
+router.get("/scenes/:id", sceneController.getSceneById);
+router.post("/scenes", sceneController.createScene);
 
 
 
-module.exports = router; */
+module.exports = router;
--- backend/services/sceneService.js
+++ backend/services/sceneService.js
@@ -8,16 +8,43 @@ TODO    getScenes
 
 function getScenes() {
     return new Promise((resolve, reject) => {
-        let SQL = "SELECT * FROM scene";
-        connectionMYSQL.query(sql, (err, result) => {
+        let sql = "SELECT * FROM scene";
+        connectionMySQL.query(sql, (err, result) => {
             if (err) reject(err);
             else resolve(result);
         })
     })
 }
 
+function getSceneById(id) {
+    return new Promise((resolve, reject) => {
+        let sql = "SELECT * FROM scene WHERE id = ?";
+        const params = [id];
+        connectionMySQL.query(sql,params, (err, results) => {
+            if (err) reject(err);
+            else resolve(results[0]);
+        });
+    });
+}
+
+function createScene (sessionId) {
+    return new Promise((resolve, reject) => {
+        let sql = `
+        INSERT INTO scene(sessionId)
+        VALUE (?)
+        `;
+        const params = [sessionId];
+        connectionMySQL.query(sql, params, (err, results) => {
+            if (err) reject (err);
+            else resolve(results); // else resolve(results[0]);
+        });
+    });
+}
+
 module.exports = {
-    getScenes
+    getScenes,
+    getSceneById,
+    createScene
 };
 
 
