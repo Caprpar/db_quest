15824e92e1409cd6840b864f8316fcc9ceda1e2a
swoerve <swoerve@gmail.com>
2025-04-30 11:35:46 +0200

    finished with endpoints

--- backend/routes/sessionRoutes.js
+++ backend/routes/sessionRoutes.js
@@ -6,25 +6,21 @@ const sessionController = require("../controllers/sessionController");
 router.get("/sessions", sessionController.getAllSessions);
 
 // GET sessions by id
-router.get("/sessions/:id");
+router.get("/sessions/:id", sessionController.getSessionsById);
 
 // GET sessions for user
-router.get("/sessions/user/:id", (req, res) => {
-  res.send("hihi");
-});
+router.get("/sessions/user/:id", sessionController.getSessionsByUser);
 
 // GET sessions by tag
-router.get("/sessions/tag/:id", (req, res) => {
-  res.send("haha");
-});
+router.get("/sessions/tags", sessionController.getSessionsByTag);
 
 // POST session to database
 router.post("/sessions", sessionController.postSession);
 
 // UPDATE session in database
-router.put("/sessions/:id", (req, res) => {});
+router.put("/sessions/:id", sessionController.updateSession);
 
 // DELETE session in database
-router.delete("/sessions/:id");
+router.delete("/sessions/:id", sessionController.deleteSession);
 
 module.exports = router;
