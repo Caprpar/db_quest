4fb24037e4f4644bc382840f628802f517e44bac
swoerve <swoerve@gmail.com>
2025-04-30 10:28:01 +0200

    all routes implemented simply

--- backend/routes/sessionRoutes.js
+++ backend/routes/sessionRoutes.js
@@ -5,22 +5,26 @@ const sessionController = require("../controllers/sessionController");
 //GET ALL sessions
 router.get("/sessions", sessionController.getAllSessions);
 
+// GET sessions by id
+router.get("/sessions/:id");
+
 // GET sessions for user
-router.get("/sessions/:user", (req, res) => {
+router.get("/sessions/user/:id", (req, res) => {
   res.send("hihi");
 });
 
 // GET sessions by tag
-router.get("/sessions/:tag", (req, res) => {
+router.get("/sessions/tag/:id", (req, res) => {
   res.send("haha");
 });
 
 // POST session to database
-router.post("/sessions", (req, res) => {
-  res.send("im getting a session");
-});
+router.post("/sessions", sessionController.postSession);
 
 // UPDATE session in database
 router.put("/sessions/:id", (req, res) => {});
+
+// DELETE session in database
+router.delete("/sessions/:id");
 
 module.exports = router;
