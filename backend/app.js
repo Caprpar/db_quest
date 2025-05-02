4c5a9c0a357decc8883ac3c1f93e44469499af4f
Caspar Danielsson <caspar.danielsson@iths.se>
2025-05-02 10:15:19 +0200

    rearrange appjs

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const cors = require("cors");
-
 const app = express();
 const port = 3000;
 
@@ -9,18 +8,17 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(express.static("public"));
 
-const sessionRoutes = require("./routes/sessionRoutes");
-app.use("/api", sessionRoutes);
-
-const userRoutes = require("./routes/userRoutes");
 const cardRoutes = require("./routes/cardRoutes");
-const sceneRoutes = require("./routes/sceneRoutes");
-app.use("/api", cardRoutes);
-app.use("/api", sceneRoutes);
-app.use("/api", userRoutes);
-
 const promptRoutes = require("./routes/promptRoutes");
+const sceneRoutes = require("./routes/sceneRoutes");
+const sessionRoutes = require("./routes/sessionRoutes");
+const userRoutes = require("./routes/userRoutes");
+
+app.use("/api", cardRoutes);
 app.use("/api", promptRoutes);
+app.use("/api", sceneRoutes);
+app.use("/api", sessionRoutes);
+app.use("/api", userRoutes);
 
 const connectionMongoDB = require("./connectionMongoDB");
 connectionMongoDB();
