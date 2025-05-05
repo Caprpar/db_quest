119257ff083e7cb07a2128afa926408120e00854
Caspar Danielsson <caspar.danielsson@iths.se>
2025-05-05 12:00:28 +0200

    create events for click and hover

--- frontend/src/components/TheHearts.vue
+++ frontend/src/components/TheHearts.vue
@@ -2,25 +2,45 @@
   import { ref, onBeforeMount } from "vue";
   import heartImg from "@/assets/heart.svg";
   const hp = ref();
-  hp.value = 10;
+  hp.value = {
+    max: 10,
+    current: 5
+  };
   let hearts = ref();
-  hearts.value = getHeartArray(hp.value);
-
+  hearts.value = getHeartArray(hp.value.current, hp.value.max);
+  let eventStatus = ref();
   onBeforeMount(() => {});
 
-  function getHeartArray(amount = 10) {
-    const heart = "./../assets/heart.svg";
+  function getHeartArray(hp, amount = 10) {
     const hearts = [];
     for (let index = 0; index < amount; index++) {
       hearts.push({ id: index, src: heartImg });
     }
     return hearts;
   }
+
+  function clickHeart(heart) {
+    eventStatus.value = `Du klicka på hjärta nr: ${heart.id}`;
+  }
+
+  function hoverHeart(heart) {
+    eventStatus.value = `Du hoverar på hjärta nr: ${heart.id}`;
+  }
+  // Funktion to preview choice
+  // Funktion to confirm choice
 </script>
 
 <template>
-  <h1>HP: {{ hp }}</h1>
-  <img v-for="heart in hearts" :key="heart.index" :src="heart.src" alt="heart" />
+  <h1>HP: {{ hp.current }}</h1>
+  <p>{{ eventStatus }}</p>
+  <img
+    v-for="heart in hearts"
+    :key="heart.id"
+    :src="heart.src"
+    @click="clickHeart(heart)"
+    @mouseover="hoverHeart(heart)"
+    alt="heart"
+  />
 </template>
 
 <style scoped>
