78df879658e0b91b3f73122aaddd49aaca5bcd8c
swoerve <swoerve@gmail.com>
2025-05-05 11:20:15 +0200

    start work on navbar

---
+++ frontend/src/components/NavBar.vue
@@ -0,0 +1,18 @@
+<script setup>
+  import { RouterLink } from "vue-router";
+
+  function startNewSession() {
+    // ask user for prompt
+  }
+</script>
+
+<template>
+  <nav>
+    <RouterLink to="/">Home</RouterLink>
+    <RouterLink to="/profile">Profile</RouterLink>
+    <RouterLink to="/browser">Browse</RouterLink>
+    <RouterLink to="/new">New</RouterLink>
+  </nav>
+</template>
+
+<style scoped></style>
