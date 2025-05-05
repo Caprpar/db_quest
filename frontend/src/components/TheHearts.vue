51bd87477a6026f2b758dda5753e030910df613c
Caspar Danielsson <caspar.danielsson@iths.se>
2025-05-05 10:59:17 +0200

    reset css

---
+++ frontend/src/components/TheHearts.vue
@@ -0,0 +1,11 @@
+<script setup>
+  console.log("hi");
+</script>
+
+<template>
+  <h1>hjärtan</h1>
+</template>
+
+<style scoped>
+  /*  */
+</style>
---
+++ frontend/src/views/HeartsView.vue
@@ -0,0 +1,9 @@
+<script setup>
+  import TheHearts from "@/components/TheHearts.vue";
+</script>
+
+<template>
+  <TheHearts />
+</template>
+
+<style></style>
