471f2dccfac2d98f0f8f6c7e1a6bd65b0999cea7
swoerve <swoerve@gmail.com>
2025-05-05 14:26:36 +0200

    added fetch for prompts

--- frontend/src/views/NewSessionView.vue
+++ frontend/src/views/NewSessionView.vue
@@ -1,18 +1,41 @@
 <script setup>
-  import { ref } from "vue";
+  import { onMounted, ref } from "vue";
 
   const test = ref("");
+
+  console.log("prompts");
+  const prompts = ref();
+  console.log("start fetch");
+  onMounted(async () => {
+    prompts.value = await fetch("http://localhost:3000/api/prompts")
+      .then((res) => {
+        console.log("in fetch");
+        console.log(res);
+        return res.json();
+      })
+      .then((data) => {
+        console.log(data);
+        return data.prompts;
+      })
+      .catch((err) => console.error(err));
+    console.log("post fetch");
+  });
+  function startStory() {
+    // create session in database, and then move user to a new scene page
+    console.log(test.value);
+  }
 </script>
 
 <template>
   <div>
     <select name="select" id="select" v-model="test">
-      <option value="test">test</option>
-      <option value="test1">test1</option>
-      <option value="test2">test2</option>
+      <option v-for="prompt in prompts" :key="prompt" :value="prompt.prompt">
+        {{ prompt.prompt }}
+      </option>
     </select>
     <input type="text" v-model="test" />
   </div>
+  <button @click="startStory">Start</button>
 </template>
 
 <style scoped>
