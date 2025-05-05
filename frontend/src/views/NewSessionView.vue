17a9820e146f4f1c29dec8448df46702cbbc18b3
swoerve <swoerve@gmail.com>
2025-05-05 12:00:31 +0200

    dropdown input

--- frontend/src/views/NewSessionView.vue
+++ frontend/src/views/NewSessionView.vue
@@ -1,17 +1,32 @@
 <script setup>
+  import { ref } from "vue";
+
   const test = ref("");
 </script>
 
 <template>
   <div>
-    <h2>Creating new story</h2>
-    <input type="text" v-model="test" />
-    <select name="" id="" v-model="test">
-      <option value="test"></option>
-      <option value="test1"></option>
-      <option value="test2"></option>
+    <select name="select" id="select" v-model="test">
+      <option value="test">test</option>
+      <option value="test1">test1</option>
+      <option value="test2">test2</option>
     </select>
+    <input type="text" v-model="test" />
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+  input {
+    border-right: none;
+    border-radius: 10px 0 0 10px;
+    width: 160px;
+    position: relative;
+    left: -200px;
+  }
+
+  select {
+    border-left: none;
+    border-radius: 10px;
+    width: 200px;
+  }
+</style>
