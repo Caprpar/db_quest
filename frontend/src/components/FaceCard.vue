6995ab809acac8ff3e1b32945b76e871d29dee3f
Caspar Danielsson <caspar.danielsson@iths.se>
2025-05-06 15:03:03 +0200

    add retrives type, score and emits name and description

---
+++ frontend/src/components/FaceCard.vue
@@ -0,0 +1,86 @@
+<script setup>
+  import { ref, defineProps, defineEmits } from "vue";
+  const emit = defineEmits(["lore-content"]);
+  const props = defineProps({
+    cardType: {
+      type: String,
+      default: null
+    },
+    cardScore: {
+      type: Number,
+      default: null
+    }
+  });
+
+  const name = ref("");
+  const description = ref("");
+
+  function getLoreContent(name, description) {
+    emit("lore-content", name, description);
+  }
+
+  console.log(props.cardType, props.cardScore);
+</script>
+
+<template>
+  <form action="" id="card">
+    <div id="score">{{ cardScore }}</div>
+    <img src="https://placehold.co/350x310" alt="" />
+    <input type="text" v-model="name" placeholder="Name" />
+    <textarea type="textarea" v-model="description" placeholder="Description" />
+    <input
+      type="submit"
+      value="Resolve"
+      @submit.prevent="getLoreContent(name, description)"
+      @click.prevent="getLoreContent(name, description)"
+    />
+  </form>
+  {{ name }}
+  {{ description }}
+</template>
+
+<style scoped>
+  #card {
+    position: relative;
+    width: 350px;
+    border: solid #8a8a8a;
+    display: flex;
+    flex-direction: column;
+  }
+  input[type="text"],
+  textarea {
+    font-size: 0.9em;
+  }
+
+  input[type="text"] {
+    border: none;
+    border-top: solid 0.5px #8a8a8a;
+    border-bottom: solid 0.5px #8a8a8a;
+    font-family: Helvetica, sans-serif;
+  }
+  textarea {
+    resize: none;
+    border: none;
+    display: flex;
+    align-items: start;
+    height: 4em;
+    font-family: Helvetica, sans-serif;
+  }
+  #score {
+    position: absolute;
+    display: flex;
+    justify-content: center;
+    font-family: Arial, Helvetica, sans-serif;
+    font-size: 1.5em;
+    color: #3f3f3f;
+    align-items: center;
+    top: 233px;
+    right: 23px;
+    background-color: white;
+    border: dashed #3f3f3f;
+    border-radius: 50%;
+    width: 50px;
+    height: 50px;
+    /* left: 100%; */
+  }
+</style>
--- frontend/src/views/CaspDev.vue
+++ frontend/src/views/CaspDev.vue
@@ -1,6 +1,7 @@
 <script setup>
   import { ref, defineEmits } from "vue";
   import HealthBar from "@/components/HealthBar.vue";
+  import FaceCard from "@/components/FaceCard.vue";
   const emit = defineEmits(["update-health"]);
   const hp = ref(0);
   hp.value = 20;
@@ -10,10 +11,15 @@
     hp.value = health;
     console.log(hp.value);
   }
+
+  function getLoreContent(name, description) {
+    console.log(name, description);
+  }
 </script>
 
 <template>
-  <HealthBar :health-points="hp" @update-health="updateHealth" />
+  <!-- <HealthBar :health-points="hp" @update-health="updateHealth" /> -->
+  <FaceCard :card-type="'problem'" card-score="9" @lore-content="getLoreContent" />
 </template>
 
 <style></style>
