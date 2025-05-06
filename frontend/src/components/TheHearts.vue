ded521a3e849b6e891eee779178697c4e7036b0e
Caspar Danielsson <caspar.danielsson@iths.se>
2025-05-06 10:16:21 +0200

    complete hearts functionallity

--- frontend/src/components/TheHearts.vue
+++ frontend/src/components/TheHearts.vue
@@ -4,15 +4,17 @@
   import faded from "@/assets/fadedHeart.svg";
   import empty from "@/assets/emptyHeart.svg";
   const hp = ref();
+  const hpDiff = ref(0);
   const hearts = ref([]);
   hp.value = {
     current: 14,
     max: 20
   };
 
-  hearts.value = getHearts(hp);
+  hearts.value = initHeartArray(hp);
 
-  function getHearts(hp) {
+  // Initialize hearts array by hp amount
+  function initHeartArray(hp) {
     let hearts = [];
     for (let index = 1; index <= hp.value.max; index++) {
       hearts.push({ id: index, src: empty });
@@ -20,21 +22,38 @@
     return hearts;
   }
 
-  function updateHearts(hover) {
-    for (const heart of hearts.value) {
-      if (hover.id > hp.value.current) {
-        heart.src = faded;
-      } else if (heart.id <= hover.id) {
-        heart.src = full;
-      } else if (hover.id <= hp.value.current && heart.id <= hp.value.current) {
-        heart.src = faded;
-      } else {
-        heart.src = empty;
-      }
-    }
+  // Retruns diff
+  function getDiff(x, y) {
+    const diff = x - y > 0 ? `+${x - y}` : `${x - y}`;
+    return diff;
   }
 
-  function drawHearts(click) {
+  // Shows a preview and update diff when player hovers over hearts
+  function previewHearts(hover) {
+    for (const heart of hearts.value) {
+      // Before current hearth
+      if (heart.id <= hp.value.current) {
+        if (heart.id <= hover.id) {
+          heart.src = full;
+        } else {
+          heart.src = faded;
+        }
+      }
+      // After current hearth
+      else {
+        if (heart.id <= hover.id) {
+          heart.src = faded;
+        } else {
+          heart.src = empty;
+        }
+      }
+    }
+    // Update diff value
+    hpDiff.value = getDiff(hover.id, hp.value.current);
+  }
+
+  // confirms hearts new value
+  function updateHearts(click) {
     hp.value.current = click.id;
     for (const heart of hearts.value) {
       if (heart.id <= click.id) {
@@ -44,6 +63,7 @@
     resetHearts();
   }
 
+  // When player leaves heart area, reset hearts to correct current hp-level
   function resetHearts() {
     for (const heart of hearts.value) {
       if (heart.id <= hp.value.current) {
@@ -52,21 +72,21 @@
         heart.src = empty;
       }
     }
+    hpDiff.value = hp.value.current;
   }
 </script>
 
 <template>
-  <h1>HP: {{ hp.current }}</h1>
   <div id="hearts" @mouseleave="resetHearts">
     <img
       v-for="heart in hearts"
-      @mouseover="updateHearts(heart)"
-      @click="drawHearts(heart)"
+      @mouseover="previewHearts(heart)"
+      @click="updateHearts(heart)"
       :key="heart.id"
       :src="heart.src"
       alt=""
     />
-    <p>{{ hp.current }}</p>
+    <p>{{ hpDiff }}</p>
   </div>
 </template>
 
