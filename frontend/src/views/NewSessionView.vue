cd61457775bbdd4a789163f2ae6e0c6dbbae9fd6
swoerve <swoerve@gmail.com>
2025-05-06 15:02:53 +0200

    finished base features of NewSession page

--- frontend/src/views/NewSessionView.vue
+++ frontend/src/views/NewSessionView.vue
@@ -6,6 +6,7 @@
   console.log("prompts");
   const prompts = ref();
   console.log("start fetch");
+
   onMounted(async () => {
     prompts.value = await fetch("http://localhost:3000/api/prompts")
       .then((res) => {
@@ -20,36 +21,74 @@
       .catch((err) => console.error(err));
     console.log("post fetch");
   });
-  function startStory() {
+  async function startStory() {
     // create session in database, and then move user to a new scene page
     console.log(test.value);
+    const data = await fetch("http://localhost:3000/api/sessions", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        sessionPrompt: test.value,
+        sessionNarrative: "",
+        sessionUserId: 1
+      })
+    })
+      .then((res) => res.json())
+      .then((data) => data);
+
+    // have to fix this later, shouldnt be a hardcoded id
+    sessionStorage.setItem("sessionId", data.insertId);
   }
 </script>
 
 <template>
-  <div>
-    <select name="select" id="select" v-model="test">
-      <option v-for="prompt in prompts" :key="prompt" :value="prompt.prompt">
-        {{ prompt.prompt }}
-      </option>
-    </select>
-    <input type="text" v-model="test" />
+  <div id="box">
+    <div id="inputselect">
+      <label for="select">Session Prompt:</label>
+      <select name="select" id="select" v-model="test">
+        <option v-for="prompt in prompts" :key="prompt" :value="prompt.prompt">
+          {{ prompt.prompt }}
+        </option>
+      </select>
+      <input type="text" v-model="test" />
+    </div>
+    <button @click="startStory">Start</button>
   </div>
-  <button @click="startStory">Start</button>
 </template>
 
 <style scoped>
+  #box {
+    margin: 1rem;
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    height: 100%;
+    align-items: center;
+  }
+
+  #inputselect {
+    width: 200px;
+  }
+
   input {
     border-right: none;
     border-radius: 10px 0 0 10px;
-    width: 160px;
+    width: 88%;
     position: relative;
-    left: -200px;
+    height: 24px;
+    top: -30px;
   }
 
   select {
+    height: 30px;
     border-left: none;
     border-radius: 10px;
-    width: 200px;
+    width: 100%;
+  }
+
+  button {
+    width: 20%;
   }
 </style>
