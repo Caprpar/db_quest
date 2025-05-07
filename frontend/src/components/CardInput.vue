08c15e1fae814f40f4ee504069ee8d925dd7e136
gapsofmind <morin.ibrahim@gmail.com>
2025-05-07 10:23:24 +0200

    CardInput added  no emit

--- frontend/src/components/CardInput.vue
+++ frontend/src/components/CardInput.vue
@@ -1,25 +1,52 @@
 <script setup>
   import { ref } from "vue";
 
-  const cardTypeInput = ref("Challenge");
   const cardValueInput = ref(1);
+  const cardTypeInput = ref("Challenge");
 
-  const onClick = () => {
-    console.log("click");
+  const selectCard = (value, type) => {
+    cardValueInput.value = value;
+    cardTypeInput.value = type;
+    console.log(cardTypeInput.value, cardValueInput.value);
   };
 </script>
 
 <template>
   <h1>Challenge</h1>
 
-  <span @click="onClick" v-for="n in 13" :key="n">
-    <input :id="`val-${n}`" type="radio" v-model="cardValueInput" :value="n" />
+  <span v-for="n in 13" :key="n">
+    <input
+      :id="`val-${n}`"
+      type="radio"
+      :checked="cardValueInput === n && cardTypeInput === 'Challenge'"
+      @click="() => selectCard(n, 'Challenge')"
+    />
     <label :for="`val-${n}`">{{ n }}</label>
   </span>
 
-  <h2>Aid</h2>
+  <h1>Aid</h1>
 
-  <h2>Asset</h2>
+  <span v-for="n in 13" :key="n">
+    <input
+      :id="`val-${n}`"
+      type="radio"
+      :checked="cardValueInput === n && cardTypeInput === 'Aid'"
+      @click="() => selectCard(n, 'Aid')"
+    />
+    <label :for="`val-${n}`">{{ n }}</label>
+  </span>
+
+  <h1>Asset</h1>
+
+  <span v-for="n in 13" :key="n">
+    <input
+      :id="`val-${n}`"
+      type="radio"
+      :checked="cardValueInput === n && cardTypeInput === 'Asset'"
+      @click="() => selectCard(n, 'Asset')"
+    />
+    <label :for="`val-${n}`">{{ n }}</label>
+  </span>
 </template>
 
 <style scoped></style>
