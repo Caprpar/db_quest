3709af0c20031cff9ad1b60f908fff9d82e73e5b
gapsofmind <morin.ibrahim@gmail.com>
2025-05-12 13:07:26 +0200

    sceneServices added in frontend

--- frontend/src/services/sceneService.js
+++ frontend/src/services/sceneService.js
@@ -4,3 +4,30 @@ const api = axios.create({
   baseURL: "http://localhost:3000/api/",
   timeout: 5000
 });
+
+export async function getScene() {
+  try {
+    const response = await api.get(`/scenes`);
+    return response.data;
+  } catch (err) {
+    console.log("Fel: ", err);
+  }
+}
+
+export async function getSceneById(id) {
+  try {
+    const response = await api.get(`/scenes/${id}`);
+    return response.data;
+  } catch (err) {
+    console.log("Fel: ", err);
+  }
+}
+
+export async function postCard(scene) {
+  try {
+    const response = await api.post(`/scenes`, scene);
+    return response.data;
+  } catch (err) {
+    console.log("Fel: ", err);
+  }
+}
