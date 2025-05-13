400f972447ac4361aacb7f3f825c0691acd307c4
Caspar Danielsson <caspar.danielsson@iths.se>
2025-05-13 11:33:04 +0200

    begin styling

--- frontend/src/components/FaceCard.vue
+++ frontend/src/components/FaceCard.vue
@@ -27,46 +27,55 @@
 </script>
 
 <template>
-  <form action="" id="card">
+  <form
+    action=""
+    id="card"
+    @submit.prevent="getLoreContent(name, description, cardSlot)"
+    @click.prevent="getLoreContent(name, description, cardSlot)"
+  >
     <div id="score">{{ cardScore }}</div>
-    <img src="" :alt="cardType" />
+    <img :src="`@/assets/${cardType.toLowerCase()}.png`" :alt="cardType" />
     <input type="text" v-model="name" placeholder="Name" />
     <textarea type="textarea" v-model="description" placeholder="Description" />
-    <input
-      type="submit"
-      value="Resolve"
-      @submit.prevent="getLoreContent(name, description, cardSlot)"
-      @click.prevent="getLoreContent(name, description, cardSlot)"
-    />
+    <!-- <input type="submit" value="Resolve" /> -->
   </form>
 </template>
 
 <style scoped>
+  form {
+    background-color: #f8f8f8;
+  }
   .selected {
     transform: scale(1.03);
-    box-shadow: 0px 0px 20px #0000006b;
+    box-shadow: 0px 0px 15px var(--dark-transparent);
+    border: none;
+    border-collapse:;
+    border: solid var(--green);
   }
   .not-selected {
     border: solid #8a8a8a60;
   }
   #card:hover {
-    transform: scale(1.03);
-    box-shadow: 0px 0px 20px #0000006b;
+    /* transform: scale(1.03); */
+    border: solid var(--accent);
+    /* box-shadow: 0px 0px 20px #0000006b; */
   }
   #card {
-    transition: 0.5s;
+    transition: 0.1s;
     position: relative;
-    width: 19em;
+    width: 100%;
+    height: 100%;
     display: flex;
     flex-direction: column;
     border-radius: 1em;
-    min-height: 25em;
   }
   input[type="text"],
   textarea {
     font-size: 0.9em;
   }
-
+  textarea {
+    border-radius: 0 0 1em 1em;
+  }
   input[type="text"] {
     border: none;
     border-top: solid 0.5px #8a8a8a;
@@ -89,8 +98,8 @@
     font-size: 1.5em;
     color: #3f3f3f;
     align-items: center;
-    top: 233px;
-    right: 23px;
+    top: 206px;
+    right: 13px;
     background-color: white;
     border: dashed #918989;
     border-radius: 50%;
