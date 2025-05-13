bd8437813ef817f5b9e5fb54a6fe6ba16e411754
swoerve <swoerve@gmail.com>
2025-05-13 10:57:03 +0200

    added ReadSessionView

---
+++ frontend/src/views/ReadSessionView.vue
@@ -0,0 +1,24 @@
+<script setup>
+  import { onMounted, ref } from "vue";
+  import { useRoute } from "vue-router";
+
+  const title = ref();
+  const narrative = ref();
+  const route = useRoute();
+  onMounted(async () => {
+    const data = await fetch(`http://localhost:3000/api/sessions/${route.params.id}`)
+      .then((res) => res.json())
+      .then((data) => data[0]);
+
+    console.log(data);
+    title.value = data.title;
+    narrative.value = data.narrative;
+  });
+</script>
+
+<template>
+  <h2>{{ title }}</h2>
+  <p>{{ narrative }}</p>
+</template>
+
+<style scoped></style>
